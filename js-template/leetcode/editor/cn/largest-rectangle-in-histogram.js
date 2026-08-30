4c18f5e3b2bf6b6716549ee4bdd8840e91616bd0
Hu Mingli <huminglilord@icloud.com>
2026-08-30 09:45:15 +0800

    --

--- js-template/leetcode/editor/cn/largest-rectangle-in-histogram.js
+++ js-template/leetcode/editor/cn/largest-rectangle-in-histogram.js
@@ -16,54 +16,17 @@ import {TreeNode} from "../common/treeNode.js";
  */
 var largestRectangleArea = function(heights) {
     var cache = [];
-    var times = new Map();
     var max = 0;
 
-    for (let i = 0; i < heights.length; i++) {
-        let cur = heights[i];
-        if (cache.length === 0) {
-            cache.push(cur);
-            times.set(cur, 1);
-            continue;
-        } else if (cache.length > 0) {
-            let cur = heights[i];
-            let curTime = 1;
-            while (cache.length) {
-                if (cur === cache[cache.length - 1]) {
-                    times.set(cur, times.get(cur) + curTime);
-                    break;
-                } else if (cur > cache[cache.length - 1]) {
-                    cache.push(cur);
-                    times.set(cur, curTime);
-                    break;
-                } else if (cur < cache[cache.length - 1]) {
-                    let last = cache.pop();
-                    let time = times.get(last);
-                    times.delete(last);
-                    if (max < last * time)
-                        max = last * time;
-                    if (cache.length && cache[cache.length - 1] > cur) {
-                        times.set(cache[cache.length - 1], (times.get(cache[cache.length - 1]) | 0) + time);
-                    } else {
-                        curTime += time;
-                    }
-                }
-            }
-            if (cache.length === 0 && cur > 0) {
-                cache.push(cur);
-                times.set(cur, curTime);
-            }
-        }
-    }
+    for (let i = 0; i <= heights.length; i++) {
+        const cur = i === heights.length ? 0 : heights[i];
 
-    while (cache.length) {
-        let last = cache.pop();
-        let time = times.get(last);
-        if (max < last * time)
-            max = last * time;
-        if (cache.length) {
-            times.set(cache[cache.length - 1], (times.get(cache[cache.length - 1]) | 0) + time);
+        while (cache.length && cur < heights[cache[cache.length - 1]]) {
+            const height = heights[cache.pop()];
+            const width = cache.length === 0 ? i : i - cache[cache.length - 1] -1;
+            max = Math.max(max, height * width);
         }
+        cache.push(i);
     }
 
     return max;
@@ -73,7 +36,9 @@ var largestRectangleArea = function(heights) {
 // your test code here
 
 // largestRectangleArea([4,2,0,3,2,4,3,4])
-largestRectangleArea([0,1,0,2,1,0,1,3,2,1,2,1])
+// largestRectangleArea([0,1,0,2,1,0,1,3,2,1,2,1])
+largestRectangleArea([2, 4])
+
 
 /*
 // @lcpr case=start
