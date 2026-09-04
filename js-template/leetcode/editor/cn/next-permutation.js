a6bc3683029efe696573638cbc79a709037217f1
mingli.hu <mingli.hu@spectrum-sys.cn>
2026-09-04 11:04:08 +0800

    --

---
+++ js-template/leetcode/editor/cn/next-permutation.js
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=javascript
+ * @lcpr version=30404
+ *
+ * [31] 下一个排列
+ */
+
+/**
+整数数组的一个 **排列**  就是将其所有成员以序列或线性顺序排列。
+
+-   例如，`arr = [1,2,3]` ，以下这些都可以视作 `arr` 的排列：`[1,2,3]`、`[1,3,2]`、`[3,1,2]`、`[2,3,1]` 。
+
+整数数组的 **下一个排列** 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 **下一个排列** 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+
+-   例如，`arr = [1,2,3]` 的下一个排列是 `[1,3,2]` 。
+-   类似地，`arr = [2,3,1]` 的下一个排列是 `[3,1,2]` 。
+-   而 `arr = [3,2,1]` 的下一个排列是 `[1,2,3]` ，因为 `[3,2,1]` 不存在一个字典序更大的排列。
+
+给你一个整数数组 `nums` ，找出 `nums` 的下一个排列。
+
+必须 **[原地](https://baike.baidu.com/item/%E5%8E%9F%E5%9C%B0%E7%AE%97%E6%B3%95)** 修改，只允许使用额外常数空间。
+
+**示例 1：**
+
+**输入：**nums = \[1,2,3\]
+**输出：**\[1,3,2\]
+
+**示例 2：**
+
+**输入：**nums = \[3,2,1\]
+**输出：**\[1,2,3\]
+
+**示例 3：**
+
+**输入：**nums = \[1,1,5\]
+**输出：**\[1,5,1\]
+
+**提示：**
+
+-   `1 <= nums.length <= 100`
+-   `0 <= nums[i] <= 100`
+*/
+
+import {ListNode} from "../common/listNode.js";
+import {TreeNode} from "../common/treeNode.js";
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    let n = nums.length - 1;
+    let start = n;
+
+    const swap = function(nums, a, b) {
+        const temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    for (; start > 0; start--) {
+        if (nums[start] > nums[start-1]) {
+            let right = start;
+            for (; right <= n; right++) {
+                if (nums[start - 1] >= nums[right])
+                    break;
+            }
+            swap(nums, start - 1, right - 1);       
+            break;
+        }
+    }
+    
+    for (let i = start; i <= n; i++) {
+        for (let j = i + 1; j <= n; j++) {
+            if (nums[i] > nums[j]) {
+                swap(nums, i, j);
+            }
+        }
+    }
+};
+// @lc code=end
+
+// your test code here
+
+// nextPermutation([2,3,1])
+nextPermutation([1,5,1])
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,3,2]\n
+// @lcpr case=end
+
+ */
+
